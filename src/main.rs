5509ef1a283760cc33bc5f2f7326639700e87ed8
Frank McSherry <fmcsherry@me.com>
2015-07-27 21:02:31 +0200

    tracking comm-rewrite timely branch

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,11 @@ extern crate getopts;
 
 use timely::progress::timestamp::RootTimestamp;
 use timely::progress::nested::Summary::Local;
-use timely::example_shared::*;
-use timely::example_shared::operators::*;
+use timely::construction::*;
+use timely::construction::operators::*;
 use timely::communication::*;
 use timely::communication::pact::Exchange;
+use timely::communication::observer::Extensions;
 use timely::drain::DrainExt;
 
 mod typedrw;
@@ -19,7 +20,7 @@ use sorting::{SegmentList, radix_sort_32};
 
 fn main () {
 
-    let source = std::env::args().skip(1).next().unwrap();
+    let filename = std::env::args().skip(1).next().unwrap();
     let strategy = std::env::args().skip(2).next().unwrap() == "process";
 
     // currently need timely's full option set to parse args
@@ -33,8 +34,136 @@ fn main () {
 
         let workers: usize = matches.opt_str("w").map(|x| x.parse().unwrap_or(1)).unwrap_or(1);
 
-        timely::initialize(std::env::args().skip(2), move |communicator| {
-            pagerank_thread(communicator, &source, workers, strategy);
+        timely::execute(std::env::args().skip(2), move |root| {
+
+            let index = root.index() as usize;
+            let peers = root.peers() as usize;
+
+            let start = time::precise_time_s();
+
+            let nodes = GraphMMap::new(&filename).nodes();
+
+            let mut segments = SegmentList::new(1024); // list of edge segments
+
+            let mut src = vec![];   // holds ranks
+            let mut deg = vec![];   // holds source degrees
+            let mut rev = vec![];   // holds (dst, deg) pairs
+            let mut trn = vec![];   // holds transposed sources
+
+            let mut going = start;
+
+            let mut input = root.subcomputation(|builder| {
+
+                let (input, edges) = builder.new_input::<(u32, u32)>();
+                let (cycle, ranks) = builder.loop_variable::<(u32, f32)>(RootTimestamp::new(20), Local(1));
+
+                let mut ranks = edges.binary_notify(&ranks,
+                                    Exchange::new(|x: &(u32,u32)| x.0 as u64),
+                                    Exchange::new(|x: &(u32,f32)| x.0 as u64),
+                                    "pagerank",
+                                    vec![RootTimestamp::new(0)],
+                                    move |input1, input2, output, notificator| {
+
+                    // receive incoming edges (should only be iter 0)
+                    while let Some((_index, data)) = input1.pull() {
+                        segments.push(data.drain_temp());
+                    }
+
+                    // all inputs received for iter, commence multiplication
+                    while let Some((iter, _)) = notificator.next() {
+
+                        let now = time::now();
+
+                        if index == 0 { println!("{}:{}:{}.{} starting iteration {}", now.tm_hour, now.tm_min, now.tm_sec, now.tm_nsec, iter.inner); }
+
+                        // if the very first iteration, prepare some stuff.
+                        // specifically, transpose edges and sort by destination.
+                        if iter.inner == 0 {
+                            let (a, b, c) = transpose(segments.finalize(), peers, nodes);
+                            deg = a; rev = b; trn = c;
+                            src = vec![0.0f32; deg.len()];
+                        }
+
+                        // record some timings in order to estimate per-iteration times
+                        if iter.inner == 0  && index == 0 { println!("src: {}, dst: {}, edges: {}", src.len(), rev.len(), trn.len()); }
+                        if iter.inner == 10 && index == 0 { going = time::precise_time_s(); }
+                        if iter.inner == 20 && index == 0 { println!("average: {}", (time::precise_time_s() - going) / 10.0 ); }
+
+                        // prepare src for transmitting to destinations
+                        for s in 0..src.len() { src[s] = 0.15 + 0.85 * src[s] / deg[s] as f32; }
+
+                        // wander through destinations
+                        let mut trn_slice = &trn[..];
+                        let mut session = output.session(&iter);
+                        for &(dst, deg) in &rev {
+                            let mut accum = 0.0;
+                            for &s in &trn_slice[..deg as usize] {
+                                accum += src[s as usize];
+                            }
+                            trn_slice = &trn_slice[deg as usize..];
+                            session.give((dst, accum));
+                        }
+
+                        for s in &mut src { *s = 0.0; }
+                    }
+
+                    // receive data from workers, accumulate in src
+                    while let Some((iter, data)) = input2.pull() {
+                        notificator.notify_at(&iter);
+                        for &(node, rank) in data.iter() {
+                            src[node as usize / peers] += rank;
+                        }
+                    }
+                });
+
+                // optionally, do process-local accumulation
+                if strategy {
+                    let local_base = workers * (index / workers);
+                    let local_index = index % workers;
+                    let mut acc = vec![0.0; (nodes / workers) + 1];   // holds ranks
+                    ranks = ranks.unary_notify(
+                        Exchange::new(move |x: &(u32,f32)| (local_base as u64 + (x.0 as u64 % workers as u64))),
+                        "aggregation",
+                        vec![],
+                        move |input, output, iterator| {
+                            while let Some((iter, data)) = input.pull() {
+                                iterator.notify_at(&iter);
+                                for &(node, rank) in data.iter() {
+                                    acc[node as usize / workers] += rank;
+                                }
+                            }
+
+                            while let Some((item, _)) = iterator.next() {
+                                output.give_at(&item, acc.drain_temp().enumerate().filter(|x| x.1 != 0.0)
+                                                         .map(|(u,f)| ((u * workers + local_index) as u32, f)));
+
+                                for _ in 0..(1 + (nodes/workers)) { acc.push(0.0); }
+                            }
+                        }
+                    );
+                }
+
+                ranks.connect_loop(cycle);
+
+                input
+            });
+
+            // introduce edges into the computation;
+            // allow mmaped file to drop
+            {
+                let graph = GraphMMap::new(&filename);
+                for node in 0..graph.nodes() {
+                    if node % peers == index {
+                        for dst in graph.edges(node) {
+                            input.give((node as u32, *dst as u32));
+                        }
+                    }
+                }
+            }
+            input.close();
+            while root.step() { }
+
+            if index == 0 { println!("elapsed: {}", time::precise_time_s() - start); }
         });
     }
     else {
@@ -75,162 +204,6 @@ fn transpose(mut edges: Vec<Vec<(u32, u32)>>, peers: usize, nodes: usize) -> (Ve
 
 // pagerank dataflow graph has a set of edges as input, and a binary vertex that for each epoch of
 // received edges initiates an iterative subcomputation to compute the pagerank.
-
-fn pagerank_thread<C>(communicator: C, filename: &String, _workers: usize, _process_aggregate: bool)
-where C: Communicator {
-    let index = communicator.index() as usize;
-    let peers = communicator.peers() as usize;
-
-    let mut root = GraphRoot::new(communicator);
-
-    let start = time::precise_time_s();
-
-    let nodes = GraphMMap::new(&filename).nodes();
-
-    let mut segments = SegmentList::new(1024); // list of edge segments
-
-    let mut src = vec![];   // holds ranks
-    let mut deg = vec![];   // holds source degrees
-    let mut rev = vec![];   // holds (dst, deg) pairs
-    let mut trn = vec![];   // holds transposed sources
-
-    let mut going = start;
-
-    let mut input = root.subcomputation(|builder| {
-
-        let (input, edges) = builder.new_input::<(u32, u32)>();
-        let (cycle, ranks) = builder.loop_variable::<(u32, f32)>(RootTimestamp::new(20), Local(1));
-
-        let mut ranks = edges.binary_notify(&ranks,
-                            Exchange::new(|x: &(u32,u32)| x.0 as u64),
-                            Exchange::new(|x: &(u32,f32)| x.0 as u64),
-                            format!("pagerank"),
-                            vec![RootTimestamp::new(0)],
-                            move |input1, input2, output, notificator| {
-
-            // receive incoming edges (should only be iter 0)
-            while let Some((_index, data)) = input1.pull() {
-                segments.push(data.drain_temp());
-            }
-
-            // all inputs received for iter, commence multiplication
-            while let Some((iter, _)) = notificator.next() {
-
-                let now = time::now();
-
-                if index == 0 { println!("{}:{}:{}.{} starting iteration {}", now.tm_hour, now.tm_min, now.tm_sec, now.tm_nsec, iter.inner); }
-
-                // if the very first iteration, prepare some stuff.
-                // specifically, transpose edges and sort by destination.
-                if iter.inner == 0 {
-                    let (a, b, c) = transpose(segments.finalize(), peers, nodes);
-                    deg = a; rev = b; trn = c;
-                    src = vec![0.0f32; deg.len()];
-                }
-
-                // record some timings in order to estimate per-iteration times
-                if iter.inner == 0  && index == 0 { println!("src: {}, dst: {}, edges: {}", src.len(), rev.len(), trn.len()); }
-                if iter.inner == 10 && index == 0 { going = time::precise_time_s(); }
-                if iter.inner == 20 && index == 0 { println!("average: {}", (time::precise_time_s() - going) / 10.0 ); }
-
-                // prepare src for transmitting to destinations
-                for s in 0..src.len() { src[s] = 0.15 + 0.85 * src[s] / deg[s] as f32; }
-
-                // wander through destinations
-                let mut trn_slice = &trn[..];
-                let mut session = output.session(&iter);
-                for &(dst, deg) in &rev {
-                    let mut accum = 0.0;
-                    for &s in &trn_slice[..deg as usize] {
-                        accum += src[s as usize];
-                    }
-                    trn_slice = &trn_slice[deg as usize..];
-                    session.give((dst, accum));
-                }
-
-                // let mut rev_slice = &rev[..];
-                // while rev_slice.len() > 0 {
-                //     // TODO: session should flush automatically...
-                //     let mut session = output.session(&iter);
-                //     let next = std::cmp::min(200_000, rev_slice.len());
-                //     for &(dst, deg) in &rev_slice[..next] {
-                //         let mut accum = 0.0;
-                //         for &s in &trn_slice[..deg as usize] {
-                //             accum += src[s as usize];
-                //         }
-                //         trn_slice = &trn_slice[deg as usize..];
-                //         session.give((dst, accum));
-                //     }
-                //     rev_slice = &rev_slice[next..];
-                // }
-
-                // clean out src to accumulate into
-                for s in &mut src { *s = 0.0; }
-            }
-
-            // receive data from workers, accumulate in src
-            while let Some((iter, data)) = input2.pull() {
-                notificator.notify_at(&iter);
-                for x in data.drain_temp() {
-                    src[x.0 as usize / 128] += x.1;
-                }
-            }
-        });
-
-        // optionally, do process-local accumulation
-        if _process_aggregate {
-            let local_base = _workers * (index / _workers);
-            let local_index = index % _workers;
-            let mut acc = vec![0.0; (nodes / _workers) + 1];   // holds ranks
-            ranks = ranks.unary_notify(
-                Exchange::new(move |x: &(u32,f32)| (local_base as u64 + (x.0 as u64 % _workers as u64))),
-                format!("Aggregation"),
-                vec![],
-                move |input, output, iterator| {
-                    while let Some((iter, data)) = input.pull() {
-                        iterator.notify_at(&iter);
-                        for x in data.drain_temp() {
-                            acc[x.0 as usize / _workers] += x.1;
-                        }
-                    }
-
-                    while let Some((item, _)) = iterator.next() {
-                        output.give_at(&item, acc.drain_temp().enumerate().filter(|x| x.1 != 0.0)
-                                                 .map(|(u,f)| ((u * _workers + local_index) as u32, f)));
-
-                        for _ in 0..(1 + (nodes/_workers)) { acc.push(0.0); }
-                    }
-                }
-            );
-        }
-
-        ranks.connect_loop(cycle);
-
-        input
-    });
-
-    // introduce edges into the computation;
-    // allow mmaped file to drop
-    {
-        let graph = GraphMMap::new(&filename);
-
-        let mut edges = Vec::new();
-        for node in 0..graph.nodes() {
-            if node % peers == index {
-                for dst in graph.edges(node) {
-                    edges.push((node as u32, *dst as u32));
-                }
-                if edges.len() > 100_000 {
-                    input.send_at(0, edges.drain_temp());
-                    root.step();
-                }
-            }
-        }
-
-        input.send_at(0, edges.drain_temp());
-    }
-    input.close();
-    while root.step() { }
-
-    if index == 0 { println!("elapsed: {}", time::precise_time_s() - start); }
-}
+//
+// fn pagerank_thread<C>(communicator: C, filename: &String, _workers: usize, _process_aggregate: bool)
+// where C: Communicator {
